b5ab50ea68aef52ee4d3ec04c164690f0bcfa755
AlexMaxHorkun <mindkilleralexs@gmail.com>
2014-11-16 09:18:03 +0200

    adjustments

---
+++ agorkun_nsystem_converter.py
@@ -0,0 +1,6 @@
+__author__ = 'Alexander Gorkun'
+__email__ = 'mindkilleralexs@gmail.com'
+
+from alexander_gorkun.gui import application
+
+application.application.start()
--- alexander_gorkun/gui/application.py
+++ alexander_gorkun/gui/application.py
@@ -1,15 +1,14 @@
 __author__ = 'Alexander Gorkun'
 __email__ = 'mindkilleralexs@gmail.com'
 
-from alexander_gorkun.gui import agnumberconverter
-from alexander_gorkun.number_converter import converter
+from alexander_gorkun.gui import agnumberconverter as __agnumberconverter
+from alexander_gorkun.number_converter import converter as __converter
 
-conv=converter.ConverterManager()
-bdConv=converter.BinaryDecimalConverter()
-conv.converters.append(bdConv)
-hdConv=converter.HexadecimalDecimalConverter()
-conv.converters.append(hdConv)
-hbConv=converter.BinaryHexadecimalConverter(bdConv, hdConv)
-conv.converters.append(hbConv)
-app=agnumberconverter.AGNumberConverterGui(conv)
-app.start()
+__conv = __converter.ConverterManager()
+__bd_conv = __converter.BinaryDecimalConverter()
+__conv.converters.append(__bd_conv)
+__hd_conv = __converter.HexadecimalDecimalConverter()
+__conv.converters.append(__hd_conv)
+__hb_conv = __converter.BinaryHexadecimalConverter(__bd_conv, __hd_conv)
+__conv.converters.append(__hb_conv)
+application = __agnumberconverter.AGNumberConverterGui(__conv)
